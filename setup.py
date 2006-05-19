1baea28df66e6d16200e981c54604c8f5ceda23a
bermi <bermi@2753b61e-1814-0410-b878-8b63ae260319>
2006-05-19 17:35:39 +0000

    Adding setup script
    git-svn-id: http://opensvn.csie.org/inflector/trunk/python@4 2753b61e-1814-0410-b878-8b63ae260319

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Inflector',
+      version='0.1',
+      description='Text Inflector',
+      license='BSD',
+      author='Bermi Ferrer',
+      author_email='inflector@bermi.org',
+      url='http://www.bermi.org/inflector/',
+     )
